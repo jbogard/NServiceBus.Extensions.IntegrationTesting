733afe9ef0e8e59bf5e38b5dde473c14b963f1f0
Jimmy Bogard <jimmy.bogard@gmail.com>
2020-07-08 11:30:56 -0500

    Adding docs

--- src/NServiceBus.Extensions.IntegrationTesting/EndpointConfigurationExtensions.cs
+++ src/NServiceBus.Extensions.IntegrationTesting/EndpointConfigurationExtensions.cs
@@ -5,9 +5,28 @@ namespace NServiceBus.Extensions.IntegrationTesting
 {
     public static class EndpointConfigurationExtensions
     {
+        /// <summary>
+        /// Configure your endpoint for integration by configuring:
+        /// - Transport to be the learning transport
+        /// - Turn off auditing
+        /// - Purge queues at startup
+        /// - Turn off all retries
+        /// </summary>
+        /// <param name="endpoint">Endpoint configuration</param>
+        /// <returns>Endpoint configuration</returns>
         public static EndpointConfiguration ConfigureTestEndpoint(this EndpointConfiguration endpoint)
             => endpoint.ConfigureTestEndpoint(null);
 
+        /// <summary>
+        /// Configure your endpoint for integration by configuring:
+        /// - Transport to be the learning transport
+        /// - Turn off auditing
+        /// - Purge queues at startup
+        /// - Turn off all retries
+        /// </summary>
+        /// <param name="endpoint">Endpoint configuration</param>
+        /// <param name="transportConfigurationAction">Transport configuration action</param>
+        /// <returns>Endpoint configuration</returns>
         public static EndpointConfiguration ConfigureTestEndpoint(this EndpointConfiguration endpoint, 
             Action<TransportExtensions<LearningTransport>> transportConfigurationAction)
         {
