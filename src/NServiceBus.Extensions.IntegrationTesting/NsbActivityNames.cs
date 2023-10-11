7c5ca1224886a5179e7f630f6bf1d09264f9afc7
Ben Brandt <bbrandt@users.noreply.github.com>
2023-10-11 17:46:05 -0500

    Remove unused constants

--- src/NServiceBus.Extensions.IntegrationTesting/NsbActivityNames.cs
+++ src/NServiceBus.Extensions.IntegrationTesting/NsbActivityNames.cs
@@ -6,12 +6,6 @@
 
         public const string OutgoingMessageActivityName = "NServiceBus.Diagnostics.SendMessage";
 
-        public const string OutgoingEventActivityName = "NServiceBus.Diagnostics.PublishMessage";
-
-        public const string SubscribeActivityName = "NServiceBus.Diagnostics.Subscribe";
-
-        public const string UnsubscribeActivityName = "NServiceBus.Diagnostics.Unsubscribe";
-
         public const string InvokeHandlerActivityName = "NServiceBus.Diagnostics.InvokeHandler";
     }
 }
